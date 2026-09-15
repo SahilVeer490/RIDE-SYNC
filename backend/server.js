afaf9791e98f9d17d964e99c0aa13229fda7869f
SahilVeer490 <sahilveer574@gmail.com>
2026-09-16 01:36:10 +0530

    Fix User model case for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const User = require("./models/User");
+const User = require("./models/user");
 const bcrypt = require("bcryptjs");
 const Ride = require("./models/Ride");
 
